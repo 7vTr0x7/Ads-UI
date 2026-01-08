a75d0b596053490fbe0993512ee84e0a13b04ec2
7vTr0x7 <7vtrox7@gmail.com>
2026-01-08 10:43:13 +0530

    vite config changes

--- MFE/products/vite.config.ts
+++ MFE/products/vite.config.ts
@@ -1,13 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [],
       },
     }),
+    tailwindcss(),
+    federation({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Products": "./src/components/Products.tsx", // Expose component
+      },
+      shared: ["react", "react-dom", "@apollo/client"],
+    }),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 6002,
+  },
+});
